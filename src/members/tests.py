9dbf82ef7f6f8e6a004729dc97076a0eb60a9160
Aleksandr Basangov <aleksandrbasangov.1994@gmail.com>
2025-01-20 19:11:08 +0200

    tests: querysetAndPaginationTestsForMembers

--- src/members/tests.py
+++ src/members/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase,Client
 from django.contrib.auth.models import User
+import datetime
 from . import api_views, models
 
 
@@ -42,5 +43,29 @@ class MemberTagTestCase(TestCase):
         self.assertEqual(response.status_code,200)
         self.assertEqual(int(response.json()["count"]),10)
 
+
+class MemberTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        for i in range(0,30):
+            models.Member.objects.create(name="test"+str(i+1), lastname="test"+str(i+1), date_of_birth=datetime.date.today())
+        return super(MemberTestCase, cls).setUpClass()
+        
+    def test_member_queryset(self):
+        members=api_views.MemberViewset().get_queryset()
+        if len(members)!=30:
+            self.fail("incorrect number of members created, needed "+str(30)+"created "+str(len(members)))
+        self.assertEqual(members[0].name,"test1")
+        self.assertEqual(members[1].name,"test2")
+    
+    def test_member_pagination(self):
+        token=get_token()
+        c=Client(HTTP_AUTHORIZATION="Token "+token)
+        response=c.get("/api/v1/members/", headers={"Accept": "*/*"},query_params={"page":1})
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(int(response.json()["count"]),20)
+        response=c.get("/api/v1/members/", headers={"Accept": "*/*"},query_params={"page":2})
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(int(response.json()["count"]),10)
 
 # Create your tests here.
